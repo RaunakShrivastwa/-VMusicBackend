2f489b5491f1cbc661ea808c47124111401a167a
RaunakShrivastwa <kumarraunak4201@gmail.com>
2023-10-31 20:15:45 +0530

    first commit

---
+++ model/Music.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const musicSchema = new mongoose.Schema({
+    artistName: {
+        type: String,
+        required: true
+    },
+    song: {
+        type: String,
+        required: true
+    },
+    intro: {
+        type: String
+    },
+    popular: Boolean,
+
+    public_Id_song: {
+        type: String
+    },
+
+    public_Id_intro: {
+        type: String
+    },
+    profile: {
+        type: String
+    },
+    public_Id_profile: {
+        type: String
+    }
+
+
+});
+
+const Music = mongoose.model('Music', musicSchema);
+
+export default Music;
